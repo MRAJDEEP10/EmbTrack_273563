93e80ed6dd6dc2d8e349b4b2d2e9fbe8c51f9efd
Mihir Rajdeep <rajdeepmihir@gmail.com>
2021-04-29 19:19:48 +0530

    minor changes

--- inc/outPWM.h
+++ inc/outPWM.h
@@ -1,13 +1,4 @@
-/**
- * @file outPWM.h
- * @author your name (you@domain.com)
- * @brief 
- * @version 0.1
- * @date 2021-04-29
- * 
- * @copyright Copyright (c) 2021
- * 
- */
+#ifndef OUTPWM_H_INCLUDED
 #define OUTPWM_H_INCLUDED
 #include <avr/io.h>
 
--- inc/readadc.h
+++ inc/readadc.h
@@ -1,16 +1,8 @@
-/**
- * @file readadc.h
- * @author Mihir Rajdeep (273563)
- * @brief 
- * @version 0.1
- * @date 2021-04-29
- * 
- * @copyright Copyright (c) 2021
- * 
- */
+
 
 #ifndef READADC_H_INCLUDED
 #define READADC_H_INCLUDED
+#include <avr/io.h>
 
 uint16_t ReadADC(uint8_t ch);
 void InitADC();
