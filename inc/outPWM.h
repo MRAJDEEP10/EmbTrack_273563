4c54f1659bf137692f69e25da5553f476ae50515
Mihir Rajdeep <rajdeepmihir@gmail.com>
2021-04-30 12:54:31 +0530

    changes

--- inc/outPWM.h
+++ inc/outPWM.h
@@ -1,14 +1,3 @@
-/**
- * @file outPWM.h
- * @author your name (you@domain.com)
- * @brief 
- * @version 0.1
- * @date 2021-04-30
- * 
- * @copyright Copyright (c) 2021
- * 
- */
-
 #ifndef OUTPWM_H_INCLUDED
 #define OUTPWM_H_INCLUDED
 #include <avr/io.h>
